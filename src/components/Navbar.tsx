e1aafe60ea2f60a6bad98223f1df9a1bfdf28559
MAlngaawy <m.alngaawy@gmail.com>
2022-08-26 21:16:28 +0200

    handle mobile styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 // @ts-ignore
 import request from "../api/youtube.ts";
 import { useStateContext } from "../contexts/ContextProvider";
+// @ts-ignore
 
 const Navbar = (): JSX.Element => {
   const { data, setData } = useStateContext();
@@ -13,6 +14,7 @@ const Navbar = (): JSX.Element => {
       <div className="navbar__container">
         <div className="navbar__logo">
           <img src="./logo.png" alt="Logo" />
+          <Icon />
         </div>
         <div className="navbar__search">
           <SearchComponent setdata={setData} />
@@ -54,3 +56,25 @@ const SearchComponent = ({ setdata }) => {
 };
 
 export default Navbar;
+
+const Icon = () => {
+  return (
+    <svg
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      viewBox="0 0 461.001 461.001"
+    >
+      <g>
+        <path
+          d="M365.257,67.393H95.744C42.866,67.393,0,110.259,0,163.137v134.728
+		c0,52.878,42.866,95.744,95.744,95.744h269.513c52.878,0,95.744-42.866,95.744-95.744V163.137
+		C461.001,110.259,418.135,67.393,365.257,67.393z M300.506,237.056l-126.06,60.123c-3.359,1.602-7.239-0.847-7.239-4.568V168.607
+		c0-3.774,3.982-6.22,7.348-4.514l126.06,63.881C304.363,229.873,304.298,235.248,300.506,237.056z"
+        />
+      </g>
+    </svg>
+  );
+};
